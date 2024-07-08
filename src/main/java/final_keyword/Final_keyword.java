e884da2fb030298ade793c5e28903dffdf2f52b1
offic <offic@DESKTOP-GLF4UNU>
2024-07-08 15:25:35 +0600

    Introduction to "final" keyword Demo.

---
+++ src/main/java/final_keyword/Final_keyword.java
@@ -0,0 +1,9 @@
+package final_keyword;
+
+public class Final_keyword {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Welcome! Introduction to \"final\" keyword Demo.");   
+    }  
+}
